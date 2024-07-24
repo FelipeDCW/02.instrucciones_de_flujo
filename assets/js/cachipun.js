1e08ee767b847ebb37b81b3e9fa84a6406337999
Felipe Del Campo <felipe.delcampo.williams@gmail.com>
2024-07-23 20:37:52 -0400

    cachipum completo

--- assets/js/cachipun.js
+++ assets/js/cachipun.js
@@ -1,6 +1,7 @@
 /* Solicitar cuantas repeticiones va a jugar */
 var repeticiones = parseInt(prompt(`cantidad de veces a jugar: `))
-
+var contmaquina = 0
+var contusuario = 0
 for (var i = 1; i <= repeticiones; i = i + 1) {
     var eleccion = prompt(`Elegir la jugada (1=piedra ,2= papel o 3=tijera): `)
 
@@ -25,41 +26,67 @@ for (var i = 1; i <= repeticiones; i = i + 1) {
     
     if (eleccion == 1) {
         if (maquina == 1) {
+            alert(`El usuario empato`)
             console.log(`El usuario empato`)
         }
         else if (maquina == 2) {
-            console.log(`La maquina gano con papel`)    
+            alert(`La maquina gano con papel`)
+            console.log(`La maquina gano con papel`)  
+            ++contmaquina  
         }
         else {
-            console.log(`La maquina perdio con tijera`)
+            alert(`El usuario gano con tijera`)
+            console.log(`El usuario gano con tijera`)
+            ++contusuario
         }
     }
 
     if (eleccion == 2) {
         if (maquina == 1) {
+            alert(`La usuario gano con papel`)
             console.log(`La usuario gano con papel`)    
+            ++contusuario
         }
         else if (maquina == 2) {
+            alert(`El usuario empato`)
             console.log(`El usuario empato`)
             
         }
         else {
+            alert(`La maquina gano con tijera`)
             console.log(`La maquina gano con tijera`)
+            ++contmaquina
         }
     }
     if (eleccion == 3) {
         if (maquina == 1) {
-            console.log(`La maquina gano con piedra`)    
+            alert(`La maquina gano con piedra`)
+            console.log(`La maquina gano con piedra`)  
+            ++contmaquina  
         }
         else if (maquina == 2) {
-            console.log(`La maquina perdio con papel`)
+            alert(`El usuario gano con papel`)
+            console.log(`El usuario gano con papel`)
+            ++contusuario
             
         }
         else {
+            alert(`El usuario empato`)
             console.log(`El usuario empato`)
             
         }
     }
+
+}
+
+if (contusuario  < contmaquina){
+    alert(`El ganador final fue la maquina con ${contmaquina} aciertos`)
+}
+else if(contusuario > contmaquina){
+    alert(`El ganador final fue el usuario con ${contusuario} aciertos`)
+}
+else{
+    alert(`El duelo fue un empate con ${contmaquina} para cada uno`)
 
 }
 
